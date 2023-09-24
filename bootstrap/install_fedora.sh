269837a78e61051f1a999049ee0b04b200f48428
mfld <10251409+mfld@users.noreply.github.com>
2023-09-24 08:21:19 +0200

    add clear log output-function and tidy up script logic

--- bootstrap/install_fedora.sh
+++ bootstrap/install_fedora.sh
@@ -7,8 +7,17 @@ ErrorExit() {
 	exit 1
 }
 
+LogInfo() {
+	printf "\n\e[1m[INFO] %s\e[0m\n" "$1"
+}
+
 i() {
 	sudo dnf install -y "$@"
+}
+
+mkd() {
+	test -d "$1" ||
+		mkdir "$1"
 }
 
 test -f /etc/fedora-release ||
@@ -23,23 +32,19 @@ sudo -v ||
 command -v tee ||
     i coreutils
 
-echo ""
-echo "Disable sudo password check"
+LogInfo "Disable sudo password check"
 echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/"$USER"
 
-echo ""
-echo "Tune the system with (sysctl)"
+LogInfo "Tune the system with (sysctl)"
 sudo tee /etc/sysctl.d/20-"$HOSTNAME".conf <<EOF
 vm.swappiness = 10 # be aggressve when swapping out of memory
 vm.vfs_cache_pressure = 50 # improve performance on systems with a large number of files
 EOF
 
-echo ""
-echo "Update the system"
+LogInfo "Update the system"
 sudo dnf update -y
 
-echo ""
-echo "Setup DNF repositories"
+LogInfo "Setup DNF repositories"
 sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 sudo tee /etc/yum.repos.d/vscodium.repo <<EOF
 [gitlab.com_paulcarroty_vscodium_repo]
@@ -52,26 +57,22 @@ gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 metadata_expire=1h
 EOF
 
-echo ""
-echo "Install multimedia plugins"
+LogInfo "Install multimedia plugins"
 sudo dnf -y group upgrade --with-optional Multimedia
 
 # https://docs.fedoraproject.org/en-US/quick-docs/rpmfusion-setup/#_enabling_the_rpm_fusion_repositories_using_command_line_utilities
-echo ""
-echo "Install from rpmfusion"
+LogInfo "Install from rpmfusion"
 i https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm # rpm fusion free
 i https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm  # rpm fusion non-free
-sudo dnf -y --allowerasing groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+sudo dnf -y --allowerasing --best groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 sudo dnf -y groupupdate sound-and-video
 
-echo ""
-echo "Install packages"
-i --allowerasing google-roboto-fonts google-roboto-fonts tilix gnome-tweaks vim-enhanced neovim ffmpeg htop ncdu perl-HTML-Parser gnome-extensions-app \
+LogInfo "Install packages"
+i --allowerasing --best google-roboto-fonts google-roboto-fonts tilix gnome-tweaks vim-enhanced neovim ffmpeg htop ncdu perl-HTML-Parser gnome-extensions-app \
   smartmontools lm_sensors bat gnome-shell-extension-appindicator mplayer libreoffice-draw iotop fio ioping python3-pip blender codium krita vim-default-editor \
   davfs2
 
-echo ""
-echo "Setup graphics"
+LogInfo "Setup graphics"
 case $(lspci|grep ' VGA '| sed -e 's/.*VGA compatible controller://') in
 	*Radeon*)
 		sudo rm -f /etc/yum.repos.d/rpmfusion-nonfree-nvidia-driver.repo
@@ -80,50 +81,41 @@ case $(lspci|grep ' VGA '| sed -e 's/.*VGA compatible controller://') in
 		i akmod-nvidia ;;
 esac
 
-# move to seperate script and source
-# same approach with synology drive?
 if [ -n "$AUTOFS" ]; then
-	echo ""
-	echo "Configure autofs"
+	LogInfo "Setup autofs"
 	i autofs
 	sudo tee /etc/auto.master <<-EOF
 	/n   /etc/autofs/n
 	EOF
-	test -d /etc/autofs ||
-		sudo mkdir /etc/autofs
+	mkd /etc/autofs
 	sudo tee /etc/autofs/n <<-EOF
 	* -rw,intr,hard,nosuid $AUTOFS/&
 	EOF
 	sudo systemctl enable --now autofs.service
 fi
 
-echo ""
-echo "Remove leaf packages"
+LogInfo "Remove leaf packages"
 sudo dnf autoremove -y
 
-echo ""
-echo "Add user to groups"
+LogInfo "Add user to groups"
 sudo usermod -aG davfs2 "$USER"
 
-echo ""
-echo "Setup lm-sensors"
+LogInfo "Setup lm-sensors"
 sudo sensors-detect --auto
 
-echo ""
-echo "Configure and install flatpaks"
+LogInfo "Configure and install flatpaks"
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install -y com.makemkv.MakeMKV com.obsproject.Studio com.spotify.Client io.typora.Typora com.spotify.Client com.synology.SynologyDrive md.obsidian.Obsidian
+flatpak install -y com.makemkv.MakeMKV com.obsproject.Studio com.spotify.Client md.obsidian.Obsidian
 
-echo ""
-echo "Autostart synology drive on user login"
-test -d ~/.config/autostart ||
-	mkdir ~/.config/autostart
-cp /var/lib/flatpak/exports/share/applications/com.synology.SynologyDrive.desktop ~/.config/autostart/
+if [ "$SYNDRIVE" ]; then
+	LogInfo "Setup synology drive"
+	flatpak install -y com.spotify.Client com.synology.SynologyDrive
+	mkd ~/.config/autostart
+	cp /var/lib/flatpak/exports/share/applications/com.synology.SynologyDrive.desktop ~/.config/autostart/
+fi
 
-echo ""
-echo "Setup systemd-tmpfiles user service"
-test -d ~/.config/user-tmpfiles.d ||
-	mkdir ~/.config/user-tmpfiles.d
+LogInfo "Setup systemd-tmpfiles user service"
+mkd ~/.config/user-tmpfiles.d
 cat <<EOF > ~/.config/user-tmpfiles.d/custom.conf
 d %h/Downloads - - - aAmM:5d -
 d %h/tmp - - - aAmM:2w -
@@ -131,18 +123,13 @@ d %h/Pictures - - - aAmM:5d -
 d %h/KritaRecorder - - - aAmM:5d -
 EOF
 
-echo ""
-echo "Setup home directories"
-test -d ~/tmp ||
-	mkdir ~/tmp
-test -d ~/git ||
-	mkdir ~/git
-test -d ~/go ||
-	mkdir ~/go
+LogInfo "Setup home directories"
+mkd ~/tmp
+mkd ~/git
+mkd ~/go
 
 systemctl --user restart systemd-tmpfiles-clean.service
 systemd-tmpfiles --user --boot --remove --create
 
-echo ""
-echo "Reboot in 5 minutes"
+LogInfo "Reboot in 5 minutes"
 shutdown -r +5
