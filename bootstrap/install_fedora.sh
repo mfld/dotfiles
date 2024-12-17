568ab9e9e9558778e10b36bf84ca418a15c15d88
mfld <10251409+mfld@users.noreply.github.com>
2024-12-17 07:16:34 +0100

    use graphics during boot

--- bootstrap/install_fedora.sh
+++ bootstrap/install_fedora.sh
@@ -77,13 +77,15 @@ i tilix gnome-tweaks vim-enhanced neovim ffmpeg htop ncdu perl-HTML-Parser gnome
   gnome-shell ffmpegthumbnailer file-roller gnome-text-editor libavcodec-freeworld nautilus xdg-user-dirs xdg-user-dirs-gtk desktop-backgrounds-gnome \
   ptyxis gnome-software gnome-system-monitor gnome-disk-utility gnome-weather @fonts mesa-dri-drivers mesa-va-drivers shotcut firefox mozilla-ublock-origin \
   mozilla-privacy-badger.noarch totem loupe wget pciutils audacity gnome-calculator gnome-characters evince tar podman node-exporter net-tools git-core fzf \
-  @multimedia usbutils gvfs-mtp
+  @multimedia usbutils gvfs-mtp plymouth-theme-breeze
 
 sudo systemctl enable libvirtd.service node_exporter.service
 sudo firewall-cmd --permanent --zone=public --add-port=9100/tcp
 
 LogInfo "Setup graphics"
 sudo systemctl set-default graphical.target
+sudo plymouth-set-default-theme breeze-text -R
+
 case $(lspci|grep ' VGA '| sed -e 's/.*VGA compatible controller://') in
 	*Radeon*)
 		sudo rm -f /etc/yum.repos.d/rpmfusion-nonfree-nvidia-driver.repo
