ded30751c8759ce01ed5b442efcb04457e43de6f
mfld <10251409+mfld@users.noreply.github.com>
2025-02-23 09:50:33 +0100

    add tree

--- bootstrap/install_fedora.sh
+++ bootstrap/install_fedora.sh
@@ -77,7 +77,7 @@ i tilix gnome-tweaks vim-enhanced neovim ffmpeg htop ncdu perl-HTML-Parser gnome
   gnome-shell ffmpegthumbnailer file-roller gnome-text-editor libavcodec-freeworld nautilus xdg-user-dirs xdg-user-dirs-gtk desktop-backgrounds-gnome \
   ptyxis gnome-software gnome-system-monitor gnome-disk-utility gnome-weather @fonts mesa-dri-drivers mesa-va-drivers shotcut firefox mozilla-ublock-origin \
   mozilla-privacy-badger.noarch totem loupe wget pciutils audacity gnome-calculator gnome-characters evince tar podman node-exporter net-tools git-core fzf \
-  @multimedia usbutils gvfs-mtp plymouth-theme-breeze plymouth-system-theme rsync fzf gnome-icon-theme nvtop
+  @multimedia usbutils gvfs-mtp plymouth-theme-breeze plymouth-system-theme rsync fzf gnome-icon-theme nvtop tree
 
 sudo systemctl enable libvirtd.service node_exporter.service
 sudo firewall-cmd --permanent --zone=public --add-port=9100/tcp
@@ -130,7 +130,7 @@ systemd-detect-virt -q ||
 LogInfo "Configure and install flatpaks"
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install --noninteractive -y com.makemkv.MakeMKV com.spotify.Client md.obsidian.Obsidian \
-  org.signal.Signal
+  org.signal.Signal com.obsproject.Studio
 
 if [ "$SYNDRIVE" ]; then
   LogInfo "Setup synology drive"
